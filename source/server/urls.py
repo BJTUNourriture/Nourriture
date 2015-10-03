3c0afc5720dad4370ee986b1d62936632bc38cf6
Julien Genoud <juliengenoud@gmail.com>
2015-10-03 10:26:32 +0800

    pyhon old generated api

--- source/server/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-
-urlpatterns = patterns('',
-    # Root URL
-    url(r'^/?$', TemplateView.as_view(template_name='home.html'), name='home'),
-)
