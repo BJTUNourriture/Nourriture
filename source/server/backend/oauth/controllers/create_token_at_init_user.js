28cda6cfd0c9c30104ea4b1702555e16a7a3dbc3
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-09 17:23:47 +0200

    create new file for refactoring

--- source/server/backend/oauth/controllers/create_token_at_init_user.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by sylflo on 10/9/15.
- */
--- source/server/backend/oauth/controllers/user.js
+++ source/server/backend/oauth/controllers/user.js
@@ -2,13 +2,9 @@
  * Created by sylflo on 9/28/15.
  */
 var User = require('../models/user');
-var Client = require('../models/client');
-var Code = require('../models/code');
-var Token = require('../models/token');
+var Create_token = require('../misc/create_token_at_init_user');
 
 exports.postUsers = function (req, res) {
-
-    var redirectUri = 'localhost:8000';
 
     var user = new User({
         username: req.body.username,
@@ -24,94 +20,9 @@ exports.postUsers = function (req, res) {
 
     });
 
-    var name_client = 'API' + user.username;
-
-    var client = new Client({
-        name: name_client,
-        id: 'this_is_my_id',
-        secret: 'this_is_my_secret',
-        userId: user._id
-    });
-
-    client.save(function (err) {
-        if (err) {
-            console.log("client save function ", user._id);
-            return res.send(err);
-        }
-    });
-
-    console.log("client id ONE", client._id);
-
-    var value_code = uid(16);
-    var code = new Code({
-        value: value_code,
-        clientId: client._id,
-        redirectUri: 'localhost:8000',
-        userId: user._id
-    });
-
-    // Save the auth code and check for errors
-    code.save(function (err) {
-        if (err) {
-            console.log("code save err");
-            return res.send(err);
-        }
-
-        //callback(null, code.value);
-    });
+    Create_token(user, res);
 
 
-
-    Code.findOne({value: value_code}, function (err, authCode) {
-        console.log("AuthCode = ", authCode, '\n\n');
-        console.log("CLient id", client._id, '\n\n');
-        //console.log("Authcode Cloent id", '\n\n');
-        if (err) {
-            console.log("code find err", err);
-            return res.send(err);
-        }
-        if (authCode === undefined) {
-            console.log("console authCode Cpode ", err);
-            return res.send(err);
-        }
-        if (client._id.toString() !== authCode.clientId) {
-            console.log("error client id Code", err);
-            return res.send(err)
-        }
-        if (redirectUri !== authCode.redirectUri) {
-            console.log("error redirectUri Code", err);
-            return res.send(err);
-        }
-
-        // Delete auth code now that it has been used
-       authCode.remove(function (err) {
-            if (err) {
-                console.log("remove thing authCode");
-                return res.send(err);
-            }
-
-            // Create a new access token
-            var token = new Token({
-                value: uid(256),
-                clientId: authCode.clientId,
-                userId: authCode.userId
-            });
-
-            // Save the access token and check for errors
-            token.save(function (err) {
-                if (err) {
-                    console.log("Token created");
-                    return res.send(err);
-                }
-
-                //callback(null, token);
-            });
-           return res.json({message: 'New user added un token give', token: token});
-
-        });
-
-
-    });
 
 
 
@@ -127,23 +38,4 @@ exports.getUsers = function (req, res) {
     });
 };
 
-function uid(len) {
-    var buf = []
-        , chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-        , charlen = chars.length;
 
-    for (var i = 0; i < len; ++i) {
-        buf.push(chars[getRandomInt(0, charlen - 1)]);
-    }
-
-    return buf.join('');
-};
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-function create_token() {
-
-}
