854e31666a33ea73f946f57fc7f6e7de329488d5
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-15 09:30:48 +0200

    test google oauth on server

--- source/server/backend/oauth/controllers/auth.js
+++ source/server/backend/oauth/controllers/auth.js
@@ -93,8 +93,14 @@ passport.use(new GoogleStrategy({
         passReqToCallback   : true
     },
     function(request, accessToken, refreshToken, profile, done) {
-        User.findOrCreate({ googleId: profile.id }, function (err, user) {
-            return done(err, user);
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Google profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Google account with a user record in your database,
+            // and return that user instead.
+            return done(null, profile);
         });
     }
 ));
