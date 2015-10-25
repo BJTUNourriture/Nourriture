4f3667448fee757e85b3f519f730cf00e6de1fe3
caille_j <caille_j@epitech.eu>
2015-10-25 20:47:36 +0000

    refactor

--- source/server/routes/index.js
+++ source/server/routes/index.js
@@ -3,47 +3,24 @@ var router = express.Router();
 var app = express();
 var path = require('path');
 
-
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
 
-// define the home page route
+// defines the home page route
 router.get('/', function(req, res) {
 	res.render('index.jade');
 });
 
+// defines the route for API doc models
 router.get('/doc-models', function(req, res) {
 	res.render('doc_models.jade', {root : "./public/doc_models/api_doc"});
 });
 
+// defines the route for API doc authentication
 router.get('/doc-auth', function(req, res) {
 	res.render('doc_auth.jade', {root : "./public/doc_auth/api_doc"});
 });
-
-
-
-// /* GET Userlist page. */
-// router.get('/userlist', function(req, res) {
-//     var db = req.db;
-//     var collection = db.get('usercollection');
-//     collection.find({},{},function(e,docs){
-//         res.render('userlist', {
-//             "userlist" : docs
-//         });
-//     });
-// });
-
-// // define the about route
-// router.get('/about', function(req, res) {
-//   res.send('NOURRITURE BASE JSON');
-// });
 
 module.exports = router;
