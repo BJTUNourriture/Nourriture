0ec680beea6dbbfe5bc1c3e947838adcc6d2a1c1
duboy_m <maxime.duboy@epitech.eu>
2015-10-26 01:46:14 -0700

    refactorisation Search/Ingredients

--- source/server/public/doc_models/api_doc/api_project.js
+++ source/server/public/doc_models/api_doc/api_project.js
@@ -1,1 +1,17 @@
-define({  "name": "Nourriture API",  "version": "0.1.0",  "description": "Endpoints for the API (all endpoints are preceded by /api)",  "apidoc": "0.2.0",  "header": {    "title": "Homepage",    "content": "<p><a href=\"http://127.0.0.1:8101/\">Go back to home page</a></p>\n"  },  "sampleUrl": false,  "generator": {    "name": "apidoc",    "time": "2015-10-25T15:36:11.154Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
+define({
+  "name": "Nourriture API",
+  "version": "0.1.0",
+  "description": "Endpoints for the API (all endpoints are preceded by /api)",
+  "apidoc": "0.2.0",
+  "header": {
+    "title": "Homepage",
+    "content": "<p><a href=\"http://127.0.0.1:8101/\">Go back to home page</a></p>\n"
+  },
+  "sampleUrl": false,
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-10-26T01:29:21.102Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
