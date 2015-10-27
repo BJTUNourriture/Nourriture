a9a6d5157c8de18039e363d80d67af1797019dbe
duboy_m <maxime.duboy@epitech.eu>
2015-10-26 20:09:00 -0700

    Search/Recipes first part, no doc

--- source/server/API/controllers/search.js
+++ source/server/API/controllers/search.js
@@ -30,6 +30,7 @@
 */
 
 var Ingredients = require('../models/ingredients');
+var Recipes = require('../models/recipes')
 
 /*
 ** POSTS
@@ -202,5 +203,119 @@ exports.postSearchIngredients = function (req, res, flag) {
 		).skip((items_page - 1) * items_number).sort(query).limit(items_number);
 	    }
 	return (1);
-	
 };
+
+exports.postSearchRecipes = function (req, res, flag) {
+
+	var title =  req.body.title;
+	var order = ""
+	if (req.body.order) {
+		var order = req.body.order
+		var order_order = req.body.order.order;
+		var order_field = req.body.order.field;
+		var query = {};
+		query[order_field] = order_order;
+	}
+	var metadata = ""
+	if (req.body.metadata){
+		metadata = req.body.metadata
+		var items_number = req.body.metadata.items;
+		var items_page = req.body.metadata.page;
+	}
+
+	var type_list = req.body.type;
+
+	//If title and types are not set
+
+	if (!title && !type_list)
+		return flag === true ? -1 : res.status(400).send({message : 'You must at least set a title or a type to search'})
+
+	if (metadata == "")
+		return flag === true ? -1 : res.status(400).send({message : 'You must set the metadata'})
+
+		//If title and tags are set
+
+	if (title && type_list){
+	
+	var Json_search = {
+				"title": { "$regex": title, "$options": "i" },
+				"tags.name": { $all: tag_list}
+			  };
+	var newjson = {metadata: {current_page: items_page, order: order, tags: tag_list, title: title}}
+
+	}
+	
+	//If only name is set
+
+	else if (title && !type_list){
+
+	var Json_search = {
+				"title": { "$regex": title, "$options": "i" }
+			  };
+	var newjson = {metadata: {current_page: items_page, order: order, title: title}}
+
+	}
+
+	//If only tags is set
+
+	else if (!title && tag_list){
+
+	var Json_search = {
+				"tags.name": { $all: tag_list}
+			  };
+	var newjson = {metadata: {current_page: items_page, order: order, tags: tag_list}}
+
+	}
+	
+	var total_page
+	Recipes.find(Json_search,
+		function (err, docs) {
+			total_page = Math.round(docs.length / items_number);
+		if (total_page <= 0)
+			total_page = 1	
+		}
+	)
+
+		if (order === ""){
+			Recipes.find(
+				Json_search
+				,
+				function (err, docs) {
+					if (err)
+						return (res.send(err));
+					else if (docs.length <= 0)
+						return (res.status(404).send({message : 'Nothing find for this search'}))
+					else{
+						newjson.metadata.total_page = total_page
+						newjson.ingredients = docs
+						return (res.json(newjson));
+					}
+				} 
+		).skip((items_page - 1) * items_number).limit(items_number);
+		}
+		else if (order != ""){
+			
+			if (order_order == "desc")
+				order_order = -1
+			else
+				order_order = 1
+			if (!order_field)
+				return flag === true ? -1 : res.status(404).send({message : 'The order.field must be set'})
+			Recipes.find(
+				Json_search,
+				function (err, docs) {
+					if (err){
+						return (res.send(err));
+						}
+					else if (docs.length <= 0){
+						return (res.status(404).send({message : 'Nothing find for this search'}))}
+					else{
+						newjson.metadata.total_page = total_page
+						newjson.ingredients = docs
+						return (res.json(newjson));
+					}
+				}
+		).skip((items_page - 1) * items_number).sort(query).limit(items_number);
+	    }
+	return (1);
+}
