871c720a3c4732c63f431488efe3621a5e2fc5f1
caille_j <caille_j@epitech.eu>
2015-10-28 22:06:58 +0000

    added modulability menu with json

--- source/web/src/app/components/sidenav-material/sidenav-material.controller.js
+++ source/web/src/app/components/sidenav-material/sidenav-material.controller.js
@@ -4,14 +4,24 @@
 angular.module('NourritureControllers', ['ngMaterial'])
 	.controller('SidenavController', SidenavController);
 
-SidenavController.$inject = ["$scope", "$timeout", "$mdSidenav", "$log"];
+SidenavController.$inject = ["$scope", "$http","$timeout", "$mdSidenav", "$log"];
 
-function SidenavController($scope, $timeout, $mdSidenav, $log)
+function SidenavController($scope, $http, $timeout, $mdSidenav, $log)
 {
 	var vm = this;
 
 	//Menu icon initialization
 	vm.menuIcon = "menu";
+
+	//Sinenav Item Informations
+	$http.get("configs/sidenavConfig.json")
+		.success(function(res) {
+			vm.sidenavItems = res;
+		})
+		.error(function(res) {
+			$log.log("no config/sidenavConfig.json found! Res data : " + res.data);
+			vm.sidenavItems = [{"name" : "CONFIG NOT FOUND", "icon" : "error"}];
+		})
 
 	vm.openLeftMenu = function() {
 		$mdSidenav('left').toggle();
