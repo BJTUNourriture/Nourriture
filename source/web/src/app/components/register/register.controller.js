48d5ecfb0f75de90832caa3f72edd2e2486daa7b
caille_j <caille_j@epitech.eu>
2015-11-06 17:56:50 +0000

    added register controller

---
+++ source/web/src/app/components/register/register.controller.js
@@ -0,0 +1,15 @@
+(function() {
+'use strict';
+
+angular.module('NourritureControllers')
+	.controller('RegisterController', RegisterController);
+
+//UserProfileController.$inject = ["$scope", "$http","$timeout", "$log"];
+
+function RegisterController()
+{
+	//var vm = this;
+
+}
+
+})();
