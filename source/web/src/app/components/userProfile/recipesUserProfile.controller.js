5dd54973d02ab242d32b6e4f6dc1edfd9146af81
caille_j <caille_j@epitech.eu>
2015-11-07 18:54:50 +0000

    changed to layout fill

--- source/web/src/app/components/userProfile/recipesUserProfile.controller.js
+++ source/web/src/app/components/userProfile/recipesUserProfile.controller.js
@@ -59,7 +59,7 @@
                 who: 'Min Li Chan',
                 when: '3:08PM',
                 notes: " I'll be in your neighborhood doing errands"
-            },
+            }
         ];
     }
 
