e799fd48bcde2af7f8bcce4e0cc43f4ffadcd896
caille_j <caille_j@epitech.eu>
2015-11-07 16:09:56 +0000

    changed form validation and work on user service

--- source/web/src/app/components/register/register.controller.js
+++ source/web/src/app/components/register/register.controller.js
@@ -4,13 +4,24 @@
 angular.module('NourritureControllers')
 	.controller('RegisterController', RegisterController);
 
-RegisterController.$inject = ["$scope", "UserService"];
+RegisterController.$inject = ["$scope", "UserService", "$log"];
 
-function RegisterController($scope, UserService)
+function RegisterController($scope, UserService, $log)
 {
 	var vm = this;
 
+	vm.submit = function() {
+		UserService.rootEndpointUsers.save({"usename" : $scope.username, "password" : $scope.password, "email" : $scope.email}, vm.userRegisteredSuccess, vm.userRegisteredFailure);
+		$log.log("kek");
+	};
 
+	vm.userRegisteredSuccess = function (data) {
+		$log.log(data);
+	};
+
+	vm.userRegisteredFailure = function (error) {
+		$log.log(error);
+	};
 }
 
 })();
