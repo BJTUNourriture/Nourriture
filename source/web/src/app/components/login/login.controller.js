6b41812a4b64259e11e15234d29ee14746983b26
caille_j <caille_j@epitech.eu>
2015-11-08 15:02:31 +0000

    login finished

---
+++ source/web/src/app/components/login/login.controller.js
@@ -0,0 +1,39 @@
+(function() {
+'use strict';
+
+angular.module('NourritureControllers')
+	.controller('LoginController', LoginController);
+
+LoginController.$inject = ["$scope", "UserService", 'toastr', '$state', "$log"];
+
+function LoginController($scope, UserService, toastr, $state, $log)
+{
+	var vm = this;
+
+	vm.submit = function() {
+		UserService
+			.user_signin
+			.save({"username" : $scope.username, "password" : $scope.password})
+			.$promise
+			.then(vm.userLoginSuccess, vm.userLoginFailure);
+	};
+
+	vm.userLoginSuccess = function (data) {
+		$log.log(data.message);
+		$state.go("main.homepage");
+	};
+
+	vm.userLoginFailure = function (data) {
+		$log.log(data.data);
+		var errorMsg = "This is odd...";
+		if (data.data.message.indexOf("username") > -1)
+			errorMsg = "Seems like you typed a  wrong username :(";
+		if (data.data.message.indexOf("password") > -1)
+			errorMsg = "Seems like you typed a  wrong password :(";
+		if (data.data.message.indexOf("email") > -1)
+			errorMsg = "Your email hasn't been verified yet, please verify it ;)";
+		toastr.error(errorMsg, 'Woops...');
+	};
+}
+
+})();
