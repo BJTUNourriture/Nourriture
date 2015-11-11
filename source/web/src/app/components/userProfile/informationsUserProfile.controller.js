48804d2332259d48361cd3c58b934081fc3eeada
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-11 18:22:12 +0800

    starting to add variable angular in the information profile for the moment there the variable are fill with hard content

--- source/web/src/app/components/userProfile/informationsUserProfile.controller.js
+++ source/web/src/app/components/userProfile/informationsUserProfile.controller.js
@@ -13,11 +13,20 @@
   informationsUserProfileController.$inject = ["$scope", '$log', '$rootScope', '$timeout'];
 
   function informationsUserProfileController($scope, $log, $rootScope, $timeout) {
-    //  console.log("Bonjour");
-    $log.debug("test informationUserProfileCOntroller", $rootScope.UserProfile);
 
-    var getUserProfile = function() {
+    var vm = this;
+
+    var getUserProfile = function () {
       $log.log("toto", $rootScope.UserProfile);
+      vm.data = $rootScope.UserProfile;
+      //Init variable for test
+      vm.data.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+      et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum \
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
+      deserunt mollit anim id est laborum";
+
+      vm.data.badges = ["un", "deux", "trois", "quatre"]
     };
 
     //Timeout in ms for the moment
