25acfb25f8e7d23cfa7d0aea51b2883092902850
caille_j <caille_j@epitech.eu>
2015-11-13 15:28:54 +0000

    add of search service

---
+++ source/web/src/app/components/Search/Search.service.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('NourritureServices')
+        .factory('SearchService', SearchService);
+
+    SearchService.$inject = ["$log", "$resource", "URL_API"];
+
+    /** @ngInject */
+    function SearchService($log, $resource, URL_API) {
+
+        var service = {
+            ingredients: $resource(URL_API + '/api/search/ingredients/')
+        };
+
+        return service;
+
+    }
+})();
