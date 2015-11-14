ccae242099b4d25d21a4307e9db5278e1fc4460a
caille_j <caille_j@epitech.eu>
2015-11-14 16:51:29 +0000

    add of dialog info for tags

--- source/web/src/app/components/createIngredient/createIngredient.controller.js
+++ source/web/src/app/components/createIngredient/createIngredient.controller.js
@@ -4,9 +4,9 @@
 angular.module('NourritureControllers')
 	.controller('CreateIngredientController', CreateIngredientController);
 
-CreateIngredientController.$inject = ["$scope", "IngredientService", 'TagsService', 'toastr',"$log"];
+CreateIngredientController.$inject = ["$scope", "IngredientService", 'TagsService', 'toastr',"$log", "$mdDialog", "$document"];
 
-function CreateIngredientController($scope, IngredientService, TagsService, toastr, $log)
+function CreateIngredientController($scope, IngredientService, TagsService, toastr, $log, $mdDialog, $document)
 {
 	var vm = this;
 
@@ -89,6 +89,31 @@ function CreateIngredientController($scope, IngredientService, TagsService, toas
 			.$promise
 			.then(vm.TagsGetNameSuccess, vm.TagsGetNameFailure));
 	}
+
+	//Dialog functions
+	vm.infosTagDialog = function(event, tag) {
+		$log.log(tag);
+		$mdDialog.show({
+		controller : vm.dialogController,
+		controllerAs : 'infosTag',
+		templateUrl: 'app/templates/dialogTemplates/tagsInfos.tmpl.html',
+		parent: angular.element($document.body),
+		locals : {tag : tag},
+		bindToController : true,
+		targetEvent: event,
+		clickOutsideToClose:true
+		})
+	};
+
+	//Controller for infosTagDialog
+	vm.dialogController = function($mdDialog) {
+		var vm = this;
+
+		vm.hide = function () {
+			$mdDialog.hide();
+		}
+	}
+
 
 }
 
