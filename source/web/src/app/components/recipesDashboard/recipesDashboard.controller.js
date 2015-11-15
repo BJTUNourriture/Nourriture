efc4f135bc6f6c9977be926e37f6d306909eeb5c
caille_j <caille_j@epitech.eu>
2015-11-15 15:47:37 +0000

    progress on recipes retrieval

--- source/web/src/app/components/recipesDashboard/recipesDashboard.controller.js
+++ source/web/src/app/components/recipesDashboard/recipesDashboard.controller.js
@@ -4,11 +4,38 @@
 angular.module('NourritureControllers')
 	.controller('RecipesDashboardController', RecipesDashboardController);
 
-RecipesDashboardController.$inject = ["$scope","$log"];
+RecipesDashboardController.$inject = ["$scope","$log", "SearchService", "toastr"];
 
-function RecipesDashboardController($scope, $log)
+function RecipesDashboardController($scope, $log, SearchService, toastr)
 {
 	var vm = this;
+
+	vm.recipes = []
+
+	//Table specs
+	vm.table = {
+		name : '',
+		metadata : {
+			"items": 10,
+			"page" : 1
+		}
+	};
+
+	vm.RecipesGetSuccess = function(data) {
+		$log.log(data);
+		vm.recipes = data;
+	}
+
+	vm.RecipesGetFailure = function (data) {
+		$log.log(data.data);
+		var errorMsg = "This is odd...";
+		if(data.data.errmsg)
+			if (data.data.errmsg.indexOf("name") > -1)
+				errorMsg = "Seems like the ingredient already exists";
+		if (data.data.message)
+			errorMsg = "This ingredient doesn't exist";
+		toastr.error(errorMsg, 'Woops...', {timeOut : 300});
+	};
 
 	//Grid tiles vars
 	vm.mouseOnTile = false;
@@ -16,6 +43,13 @@ function RecipesDashboardController($scope, $log)
 	vm.mouseOnTileSet = function(state) {
 		vm.mouseOnTile = state;
 	}
+
+	//Init data
+	SearchService
+	.recipes
+	.save(vm.table)
+	.$promise
+	.then(vm.RecipesGetSuccess, vm.RecipesGetFailure)
 }
 
 })();
