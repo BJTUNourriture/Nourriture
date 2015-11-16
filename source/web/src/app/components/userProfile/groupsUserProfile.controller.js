66767e2e1c93ff88a96513488fd37d35e1495351
caille_j <caille_j@epitech.eu>
2015-11-16 18:22:56 +0000

    changed timeout on several controllers

--- source/web/src/app/components/userProfile/groupsUserProfile.controller.js
+++ source/web/src/app/components/userProfile/groupsUserProfile.controller.js
@@ -66,7 +66,7 @@
 
 
     //Timeout in ms for the moment
-    $timeout(getUserProfile, 300);
+    $timeout(getUserProfile, 1000);
   }
 
 })();
--- source/web/src/app/components/userProfile/parametersIngredientsUserProfile.controller.js
+++ source/web/src/app/components/userProfile/parametersIngredientsUserProfile.controller.js
@@ -66,7 +66,7 @@
 
 
     //Timeout in ms for the moment
-    $timeout(getUserProfile, 700);
+    $timeout(getUserProfile, 1000);
 
 
   }
