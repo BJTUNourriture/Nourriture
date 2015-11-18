42e706c95292cda34dd4815c66eafa0869e2cea2
caille_j <caille_j@epitech.eu>
2015-11-18 16:25:15 +0000

    chnage dest folder and file name on upload

--- source/server/API/controllers/uploads.js
+++ source/server/API/controllers/uploads.js
@@ -1,7 +1,16 @@
 // API/controllers/uploads.js
 
+var path = require('path');
 var multer = require('multer');
-var recipe_thumbnail_photo_upload = multer({dest : '../web/src/assets/recipes/'}).single('recipe_thumbnail_picture');
+var recipe_thumbnail_photo_storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../web/src/assets/recipes/thumbnail_pics')
+  },
+  filename: function (req, file, cb) {
+    cb(null,  Date.now() + '-' + file.fieldname + path.extname(file.originalname))
+  }
+});
+var recipe_thumbnail_photo_upload = multer({ storage : recipe_thumbnail_photo_storage}).single('recipe_thumbnail_picture');
 
 exports.postRecipeThumbnailUrl = function(req, res) {
 	recipe_thumbnail_photo_upload(req, res, function(err) {
