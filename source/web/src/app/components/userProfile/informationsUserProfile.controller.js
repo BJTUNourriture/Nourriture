4bd4556bdfd3e262bd8aed7d1f0023bb6a6eabe1
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-20 16:29:06 +0800

    open dialgo when clicking on ingredient in information controller

--- source/web/src/app/components/userProfile/informationsUserProfile.controller.js
+++ source/web/src/app/components/userProfile/informationsUserProfile.controller.js
@@ -10,9 +10,9 @@
     .module('NourritureControllers')
     .controller('informationsUserProfileController', informationsUserProfileController);
 
-  informationsUserProfileController.$inject = ["$scope", '$log', '$rootScope', '$timeout'];
+  informationsUserProfileController.$inject = ["$scope", '$log', '$rootScope', '$timeout', '$mdDialog', '$document'];
 
-  function informationsUserProfileController($scope, $log, $rootScope, $timeout) {
+  function informationsUserProfileController($scope, $log, $rootScope, $timeout, $mdDialog, $document) {
 
     var vm = this;
     vm.extend = {};
@@ -22,7 +22,7 @@
       vm.data = $rootScope.UserProfile;
       //Init variable for test
       vm.data.gender = "male";
-     /* vm.data.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+      /* vm.data.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum \
        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
@@ -33,10 +33,11 @@
        deserunt mollit anim id est laborum";*/
 
       vm.data.badge = ["un", "deux", "trois", "quatre", "cinq"];
-     /* vm.data.like = [{name_ingredient: "one"}, {name_ingredient: "two"}, {name_ingredient: "three"}, {name_ingredient: "four"},
-        {name_ingredient: "five"}, {name_ingredient: "six"}, {name_ingredient: "seven"}, {name_ingredient: "eight"},
-        { name_ingredient: "nine" }, { name_ingredient: "ten" }, { name_ingredient: "eleven" }, { name_ingredient: "titi" }, { name_ingredient: "thirteen" }, { name_ingredient: "fourteen" }, { name_ingredient: "sixteen" }, { name_ingredient: "seventeen" }];
-    */  vm.data.dislike = [{ name_ingredient: "one" }, { name_ingredient: "choux fleur" }, { name_ingredient: "choux de brus" }, { name_ingredient: "carotte" }];
+      /* vm.data.like = [{name_ingredient: "one"}, {name_ingredient: "two"}, {name_ingredient: "three"}, {name_ingredient: "four"},
+       {name_ingredient: "five"}, {name_ingredient: "six"}, {name_ingredient: "seven"}, {name_ingredient: "eight"},
+       { name_ingredient: "nine" }, { name_ingredient: "ten" }, { name_ingredient: "eleven" }, { name_ingredient: "titi" }, { name_ingredient: "thirteen" }, { name_ingredient: "fourteen" }, { name_ingredient: "sixteen" }, { name_ingredient: "seventeen" }];
+       */
+      vm.data.dislike = [{name_ingredient: "one"}, {name_ingredient: "choux fleur"}, {name_ingredient: "choux de brus"}, {name_ingredient: "carotte"}];
       vm.data.religion = [{name: "christian"}];
       vm.data.alergy = [{name: "eggs"}, {name: "milk"}];
       vm.data.followed_by = [{username: "thomas"}, {username: "vincent"}, {username: "top chef"}];
@@ -45,11 +46,39 @@
       vm.data.recipe_post = [{name: "Grilled duck"}, {name: "baguette"}];
       vm.data.recipe_like = [{name: "marmelade"}, {name: "cantonese rice"}];
 
+      console.log("informationUserProfile ", vm.data.like);
+
       $scope.$watch(angular.bind($rootScope.UserProfile, function () {
         return $rootScope.UserProfile;
       }), function (newVal) {
         vm.data = newVal;
       }, true);
+    };
+
+
+    vm.infosIngredientDialog = function (event, ingredient) {
+
+      /*ingredient.calories = 100;
+      ingredient.fat = 100;*/
+
+
+      $mdDialog.show({
+        controller: vm.dialogController,
+        controllerAs: "infosIngredient",
+        templateUrl: 'app/templates/dialogTemplates/ingredientInfos.tmpl.html',
+        parent: angular.element($document.body),
+        locals: {ingredient: ingredient},
+        bindToController: true,
+        targetEvent: event,
+        clickOutsideToClose: true
+      })
+    };
+    vm.dialogController = function ($mdDialog) {
+      var vm = this;
+
+      vm.hide = function () {
+        $mdDialog.hide();
+      }
     };
 
 
