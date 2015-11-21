ec38868d87eb8e0d255f39a6987e9e2b5e68daa8
Julien <juliengenoud@gmail.com>
2015-11-21 18:51:23 +0800

    charger userrecipes from dataset

--- source/web/src/app/components/userProfile/recipesUserProfile.controller.js
+++ source/web/src/app/components/userProfile/recipesUserProfile.controller.js
@@ -10,31 +10,42 @@
   .module('NourritureControllers')
   .controller('RecipesUserProfileController', RecipesUserProfileController);
 
-  RecipesUserProfileController.$inject = ["$scope", "$log", "$rootScope", "$timeout", "RecipeService"];
+  RecipesUserProfileController.$inject = ["$scope", "$log", "$rootScope", "$timeout", "SearchService"];
 
-  function RecipesUserProfileController($scope, $log, $rootScope, $timeout, RecipeService) {
+  function RecipesUserProfileController($scope, $log, $rootScope, $timeout, SearchService) {
     var vm = this;
 
-
     var updateRecipes = function () {
-      vm.data = $rootScope.UserProfile;
+      vm.recipes = [];//$rootScope.UserProfile;
+      //vm.recipes = [];
+      //vm.recipes = recipes
 
-      vm.data.recipetitle = "Tarte aux pommes";
-      vm.data.image = 'assets/images/recipesdummy/tarte.jpg';
+      // DATA SET 1
+      //vm.recipes = [{recipetitle:"Tarte aux pommes", image:"assets/images/recipesdummy/tarte.jpg"}];
 
-      $scope.$watch(angular.bind($rootScope.UserProfile, function () {
-        return $rootScope.UserProfile;
-      }), function (newVal) {
-        vm.data = newVal;
-      }, true);
+      // DATA SET 2
+      vm.recipes = [{recipetitle:"Tarte aux pommes", image:"assets/images/recipesdummy/tarte.jpg"},
+        {recipetitle:"Tripes à la mode de Caen", image:"assets/images/recipesdummy/tripe.jpg"},
+        {recipetitle:"Coquilles saint jacques marinées au citron", image:"assets/images/recipesdummy/jacques.jpg"}
+      ];
+
+      // DATA SET 3
+      // vm.recipes = [{recipetitle:"Tarte aux pommes", image:"assets/images/recipesdummy/tarte.jpg"},
+      // {recipetitle:"Tripes à la mode de Caen", image:"assets/images/recipesdummy/tripe.jpg"},
+      // {recipetitle:"Coquilles saint jacques marinées au citron", image:"assets/images/recipesdummy/jacques.jpg"},
+      // {recipetitle:"Coquilles saint jacques marinées au citron", image:"assets/images/recipesdummy/jacques.jpg"},
+      // {recipetitle:"Coquilles saint jacques marinées au citron", image:"assets/images/recipesdummy/jacques.jpg"}
+      // ];
     };
 
 
     // avoid lint
-    RecipeService += 1;
+    SearchService += 1;
     // vm.recipeSuccess = function (data) {
     //   $log.log(data._id);
     //   vm.data = $rootScope.UserProfile;
+    //    $log.log(data);
+    //    vm.recipes = data.recipes;
     //
     //   // bind recipes datas from server
     //   //        vm.data.recipetitle = data.title;
@@ -75,6 +86,13 @@
 
 
     $timeout(updateRecipes, 700);
+
+
+    // SearchService
+    // .recipes
+    // .save(vm.table)
+    // .$promise
+    // .then(vm.recipeSuccess, vm.recipeError)
 
     //      bind with with server after.
     //      $timeout(getRecipes, 300);
