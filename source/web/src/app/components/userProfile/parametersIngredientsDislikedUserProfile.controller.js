248e0700d51aa4fd86865044b676a9d5a231ba41
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-22 17:01:13 +0800

    add dislike just copy past like in parameters

--- source/web/src/app/components/userProfile/parametersIngredientsDislikedUserProfile.controller.js
+++ source/web/src/app/components/userProfile/parametersIngredientsDislikedUserProfile.controller.js
@@ -1,7 +1,4 @@
 /**
- * Created by sylflo on 11/22/15.
- */
-/**
  * Created by sylflo on 11/15/15.
  */
 
@@ -21,7 +18,7 @@
 
       vm.data = $rootScope.UserProfile;
 
-      vm.names_ingredient = vm.data.dislike_chips;
+      vm.names_ingredient = vm.data.like_chips;
 
 
       vm.selectedItemChip = null;
@@ -34,10 +31,11 @@
 
       new_chip.id_ingredient = original._id;
       new_chip.name_ingredient = original.name;
-      $rootScope.UserProfile.dislike_chips_id.push(original._id);
+      $rootScope.UserProfile.like_chips_id.push(original._id);
       if (vm.names_ingredient.indexOf(original.name) == -1) {
-        vm.data.dislike.push(new_chip);
+        vm.data.like.push(new_chip);
       }
+
       return (original.name);
     };
 
