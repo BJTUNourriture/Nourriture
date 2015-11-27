900511dc3ae7243fff4fb58b1dfc24d24e142739
caille_j <caille_j@epitech.eu>
2015-11-27 15:27:16 +0000

    added descriptions instead of comments

--- source/server/test/API/tests-ingredient.js
+++ source/server/test/API/tests-ingredient.js
@@ -30,142 +30,157 @@ describe('/api/ingredients', function() {
 	/*Testing vars*/
 	var ingredient_id = "";
 
-	/*Begin Ingredient Creation*/
-	it('should successfully create an ingredient', function(done) {
-	  	var profile = {
-			name: 'test'
-		};
+	describe("Ingredients Creation", function () {
+		it('should successfully create an ingredient', function(done) {
+		  	var profile = {
+				name: 'test'
+			};
 
-		request(url)
-			.post('/api/ingredients')
-			.send(profile)
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(200);
-				done();
-			});
+			request(url)
+				.post('/api/ingredients')
+				.send(profile)
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(200);
+					done();
+				});
+		});
+
+		it('should not create an ingredient with the same name', function(done) {
+		  	var profile = {
+				name: 'test'
+			};
+
+			request(url)
+				.post('/api/ingredients')
+				.send(profile)
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(400);
+					done();
+				});
+		});
 	});
 
-	it('should not create an ingredient with the same name', function(done) {
-	  	var profile = {
-			name: 'test'
-		};
+	describe('Ingredients Retrieval', function() {
+		it('should retrieve all the ingredients' , function(done) {
 
-		request(url)
-			.post('/api/ingredients')
-			.send(profile)
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(400);
-				done();
-			});
-	});
-	/*End Ingredient Creation*/
+			request(url)
+				.get('/api/ingredients/')
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(200);
+					res.body.should.be.lengthOf(1);
+					ingredient_id = res.body[0]._id;
+					done();
+				});
+		});
 
-	/*Begin Ingredient Retrieval*/
-	it('should retrieve all the ingredients' , function(done) {
+		it('should retrieve an ingredient by it\'s name' , function(done) {
 
-		request(url)
-			.get('/api/ingredients/')
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(200);
-				res.body.should.be.lengthOf(1);
-				ingredient_id = res.body[0]._id;
-				done();
-			});
-	});
+			request(url)
+				.get('/api/ingredients/name/test')
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(200);
+					res.body[0].name.should.be.equal("test");
+					ingredient_id = res.body[0]._id;
+					done();
+				});
+		});
 
-	it('should retrieve an ingredient by it\'s name' , function(done) {
+		it('should retrieve an ingredient by it\'s partial name' , function(done) {
 
-		request(url)
-			.get('/api/ingredients/name/test')
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(200);
-				res.body[0].name.should.be.equal("test");
-				ingredient_id = res.body[0]._id;
-				done();
-			});
-	});
+			request(url)
+				.get('/api/ingredients/name/es')
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(200);
+					res.body[0].name.should.be.equal("test");
+					done();
+				});
+		});
 
-	it('should retrieve an ingredient by it\'s partial name' , function(done) {
+		it('should return 404 on non-existant name' , function(done) {
 
-		request(url)
-			.get('/api/ingredients/name/es')
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(200);
-				res.body[0].name.should.be.equal("test");
-				done();
-			});
-	});
+			request(url)
+				.get('/api/ingredients/name/ass')
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(404);
+					done();
+				});
+		});
 
-	it('should return 404 on non-existant name' , function(done) {
+		it('should retrieve an ingredient by it\'s Id' , function(done) {
 
-		request(url)
-			.get('/api/ingredients/name/ass')
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(404);
-				done();
-			});
-	});
+			request(url)
+				.get('/api/ingredients/id/'+ ingredient_id)
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(200);
+					res.body.name.should.be.equal("test");
+					done();
+				});
+		});
 
-	it('should retrieve an ingredient by it\'s Id' , function(done) {
+		it('should return 404 on non-existant Id' , function(done) {
 
-		request(url)
-			.get('/api/ingredients/id/'+ ingredient_id)
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(200);
-				res.body.name.should.be.equal("test");
-				done();
-			});
+			request(url)
+				.get('/api/ingredients/id/'+ mongoose.Types.ObjectId())
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(404);
+					done();
+				});
+		});
 	});
 
-	it('should return 404 on non-existant Id' , function(done) {
+	describe('Ingredients Update', function() {
+		it('should update all the ingredient\'s fields by Id' , function(done) {
 
-		request(url)
-			.get('/api/ingredients/id/'+ mongoose.Types.ObjectId())
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(404);
-				done();
-			});
+			request(url)
+				.put('/api/ingredients/id/'+ ingredient_id)
+				.send({
+					"name" : "test2",
+					"description" : "test",
+					"calories" : 1,
+					"fat" : 1,
+					"carbohydrates" : 1,
+					"proteins" : 1
+				})
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(200);
+					res.body.message.should.be.equal("Ingredient successfully updated!");
+					done();
+				});
+		});
+
+		it('should not update an ingredient with an unknown key by Id' , function(done) {
+
+			request(url)
+				.put('/api/ingredients/id/'+ ingredient_id)
+				.send({
+					"top" : "kek",
+				})
+				.end(function(err, res) {
+					  if (err)
+						throw err;
+					res.status.should.be.equal(400);
+					done();
+				});
+		});
 	});
-	/*End Ingredient Retrieval*/
-
-	/*Begin Ingredient Update*/
-	it('should update all the ingredient\'s fields by Id' , function(done) {
-
-		request(url)
-			.put('/api/ingredients/id/'+ ingredient_id)
-			.send({
-				"name" : "test2",
-				"description" : "test",
-				"calories" : 1,
-				"fat" : 1,
-				"carbohydrates" : 1,
-				"proteins" : 1
-			})
-			.end(function(err, res) {
-				  if (err)
-					throw err;
-				res.status.should.be.equal(200);
-				res.body.message.should.be.equal("Ingredient successfully updated!");
-				done();
-			});
-	});	
-	/*End Ingredient Update*/
 
 	if (standalone_test)
 	{
