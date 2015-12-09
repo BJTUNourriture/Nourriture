62d1b09d9e70f07ec3ecea834c292fb942157e7a
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-12-09 16:24:53 +0800

    anonymous useractivity first on login activity

--- source/android/app/src/main/java/cn/bjtu/nourriture/AnonymousActivity.java
+++ source/android/app/src/main/java/cn/bjtu/nourriture/AnonymousActivity.java
@@ -3,18 +3,78 @@ package cn.bjtu.nourriture;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import cn.bjtu.nourriture.fragments.GroupFragment;
+import cn.bjtu.nourriture.fragments.IngredientFragment;
+import cn.bjtu.nourriture.fragments.LoginFragment;
+import cn.bjtu.nourriture.fragments.RecipeFragment;
+import cn.bjtu.nourriture.fragments.UserFragement;
 
 /**
  * Created by sylflo on 12/9/15.
  */
-public class AnonymousActivity  extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class AnonymousActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    //Defining Variables
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
+    LoginFragment mLoginFragment = new LoginFragment();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.anonymous_layout);
+
+
+        // Initializing Toolbar and setting it as the actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+
+        // SET USER SELECTED
+        navigationView.getMenu().getItem(0).setChecked(true);
+        android.support.v4.app.FragmentTransaction trans1 = getSupportFragmentManager().beginTransaction();
+        trans1.replace(R.id.frame, LoginFragment.newInstance());
+        trans1.commit();
     }
 
     @Override
@@ -40,7 +100,41 @@ public class AnonymousActivity  extends AppCompatActivity implements NavigationV
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        drawerLayout.closeDrawers();
+
+        //Check to see which item was being clicked and perform appropriate action
+        switch (menuItem.getItemId()) {
+
+
+            //Replacing the main content with ContentFragment Which is our Inbox View;
+            case R.id.user:
+                toolbar.setTitle(getString(R.string.user));
+                android.support.v4.app.FragmentTransaction trans1 = getSupportFragmentManager().beginTransaction();
+                trans1.replace(R.id.frame, UserFragement.newInstance());
+                trans1.commit();
+                break;
+       /*     case R.id.ingredients:
+                toolbar.setTitle(getString(R.string.ingredients));
+                android.support.v4.app.FragmentTransaction trans2 = getSupportFragmentManager().beginTransaction();
+                trans2.replace(R.id.frame, IngredientFragment.newInstance());
+                trans2.commit();
+                break;
+            case R.id.recipes:
+                toolbar.setTitle(getString(R.string.recipes));
+                android.support.v4.app.FragmentTransaction trans3 = getSupportFragmentManager().beginTransaction();
+                trans3.replace(R.id.frame, RecipeFragment.newInstance());
+                trans3.commit();
+                break;
+            case R.id.groups:
+                toolbar.setTitle(getString(R.string.groups));
+                android.support.v4.app.FragmentTransaction trans4 = getSupportFragmentManager().beginTransaction();
+                trans4.replace(R.id.frame, GroupFragment.newInstance());
+                trans4.commit();
+                break;
+            default:
+                break;*/
+        }
+        return true;
     }
 }
