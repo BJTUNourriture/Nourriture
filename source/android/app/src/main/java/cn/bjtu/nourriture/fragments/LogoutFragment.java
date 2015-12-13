b6672abbd13ff4486ee3e527f028cf7bfb7efcc0
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-12-13 13:05:50 +0800

    add logout

--- source/android/app/src/main/java/cn/bjtu/nourriture/fragments/LogoutFragment.java
+++ source/android/app/src/main/java/cn/bjtu/nourriture/fragments/LogoutFragment.java
@@ -1,6 +1,13 @@
 package cn.bjtu.nourriture.fragments;
 
-import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+import cn.bjtu.nourriture.AnonymousActivity;
+import cn.bjtu.nourriture.R;
 
 /**
  * Created by sylflo on 12/13/15.
@@ -10,5 +17,21 @@ public class LogoutFragment extends Fragment {
     public static LogoutFragment newInstance() {
         LogoutFragment fragment = new LogoutFragment();
         return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences preferences = getActivity().getSharedPreferences(
+                "GLOBAL", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove(getString(R.string.username_pref));
+        editor.remove(getString(R.string.token_pref));
+        editor.remove(getString(R.string.user_id_pref));
+        editor.apply();
+
+        Intent intent = new Intent(getActivity(), AnonymousActivity.class);
+        startActivity(intent);
     }
 }
