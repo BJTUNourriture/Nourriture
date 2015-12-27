e9782ac3f7af4b5ef9acac0f9a2c3fc84f01191b
Maxime Duboy <maxime.duboy@epitech.eu>
2015-12-27 16:26:11 +0800

    Ingredient Add AbstractFragment

---
+++ source/android/app/src/main/java/cn/bjtu/nourriture/adapters/IngredientsAdapter.java
@@ -0,0 +1,7 @@
+package cn.bjtu.nourriture.adapters;
+
+/**
+ * Created by Tagzz on 27/12/2015.
+ */
+public class IngredientsAdapter {
+}
---
+++ source/android/app/src/main/java/cn/bjtu/nourriture/fragments/IngredientAbstractFragment.java
@@ -0,0 +1,7 @@
+package cn.bjtu.nourriture.fragments;
+
+/**
+ * Created by Tagzz on 27/12/2015.
+ */
+public class IngredientAbstractFragment {
+}
