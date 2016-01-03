c53d098aca0b56e11ccdc09ffd5342a2087b6d73
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-01-03 16:48:15 +0800

    change timeout

--- source/web/src/app/components/userProfile/parametersUserProfile.controller.js
+++ source/web/src/app/components/userProfile/parametersUserProfile.controller.js
@@ -28,18 +28,6 @@
        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
        deserunt mollit anim id est laborum";*/
 
-      vm.data.badge = ["un", "deux", "trois", "quatre"];
-      /*vm.data.like = [{name_ingredient: "one"}, {name_ingredient: "two"}, {name_ingredient: "three"}, {name_ingredient: "four"},
-       {name_ingredient: "five"}, {name_ingredient: "six"}, {name_ingredient: "seven"}, {name_ingredient: "eight"},
-       {name_ingredient: "nine"}, {name_ingredient: "ten"}, {name_ingredient: "eleven"}, {name_ingredient: "twelve"}];*/
-     /* vm.data.dislike = [{name_ingredient: "one"}, {name_ingredient: "choux fleur"}, {name_ingredient: "choux de brus"}];*/
-      vm.data.religion = [{name: "christian"}];
-     /* vm.data.alergy = [{name: "eggs"}, {name: "milk"}];*/
-      vm.data.followed_by = [{username: "thomas"}, {username: "vincent"}, {username: "top chef"}];
-      vm.data.follow = [{username: "fanny"}, {username: "giselle"}, {username: "gertrude"}];
-      //vm.data.joined_groups = [{name: "freedom to opressed vegetables"}, {name: "Faites du Fruuuiiiitt!!"}];
-      vm.data.recipe_post = [{name: "Grilled duck"}, {name: "baguette"}];
-      vm.data.recipe_like = [{name: "marmelade"}, {name: "cantonese rice"}];
 
       vm.data.confirmEmail = vm.data.email;
 
@@ -52,7 +40,7 @@
     };
 
     //Timeout in ms for the moment
-    $timeout(getUserProfile, 1000);
+    $timeout(getUserProfile, 2000);
 
 
     vm.updateProfile = function () {
